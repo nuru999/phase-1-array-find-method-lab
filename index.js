37d082d1d3ba4d09cbe91f96e96cec958405af5e
nuru999 <70575815+nuru999@users.noreply.github.com>
2023-10-02 19:58:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const winningYearObj = record.find(game => game.result === "W");
+  
+    if (winningYearObj) {
+      return winningYearObj.year;
+    } else {
+      return undefined;
+    }
+  }
+  
